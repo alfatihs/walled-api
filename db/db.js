f5c68bbcf32b0a60495216dac6a0cbc757f00d46
Azmi Alfatih S <azmi.alfatihs@gmail.com>
2024-12-18 14:40:10 +0700

    use db neon again

--- db/db.js
+++ db/db.js
@@ -1,15 +1,15 @@
 //digunakan untuk ngebuat pool nya.const Pool = require("pg").Pool;
 const Pool = require("pg").Pool;
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-
 // const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// })
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//   });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
   
   module.exports = pool;
